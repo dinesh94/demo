c74c02e6e13220a8519bf5cff07386cec89fa77b
Dinesh <dinesh.bhavsar@siemens.com>
2018-04-11 15:29:37 -0400

    first commit

---
+++ HelloWorld.js
@@ -0,0 +1,17 @@
+var http = require("http");
+
+var b = http.createServer(function (request, response) {
+   // Send the HTTP header 
+   // HTTP Status: 200 : OK
+   // Content Type: text/plain
+   response.writeHead(200, {'Content-Type': 'text/plain'});
+   
+   // Send the response body as "Hello World"
+   response.end('Hello Neha, This is just a simple program :) \n');
+   response.end('Hello World .. \n');
+}); 
+
+b.listen(8081);
+
+// Console will print the message
+console.log('Server running at http://127.0.0.1:8081/');
